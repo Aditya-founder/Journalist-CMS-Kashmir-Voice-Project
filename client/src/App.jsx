95b8584d4337a5333fb73af214186cc46973d3ce
theNahraf <farhan.ug23@nsut.ac.in>
2025-01-25 01:07:10 +0530

    create vite and setup express and tailwind

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,43 +1,30 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import About from './pages/About';
-import SignIn from './pages/SignIn';
-import Dashboard from './pages/Dashboard';
-import Projects from './pages/Projects';
-import SignUp from './pages/SignUp';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import PrivateRoute from './components/PrivateRoute';
-import OnlyAdminPrivateRoute from './components/OnlyAdminPrivateRoute';
-import CreatePost from './pages/CreatePost';
-import UpdatePost from './pages/UpdatePost';
-import PostPage from './pages/PostPage';
-import ScrollToTop from './components/ScrollToTop';
-import Search from './pages/Search';
+import React from 'react'
+import { BrowserRouter, Link } from 'react-router-dom'
+import {Button, Navbar} from 'flowbite-react'
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Dashboard from './pages/Dashboard'
+import Signup from './pages/Signup'
+import Signin from './pages/Signin'
+import Projects from './pages/Projects'
+import Header from './components/Header'
 
-export default function App() {
+const App = () => {
   return (
-    <BrowserRouter>
-      <ScrollToTop />
-      <Header />
+    <div>
+      <Header/>
       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/about' element={<About />} />
-        <Route path='/sign-in' element={<SignIn />} />
-        <Route path='/sign-up' element={<SignUp />} />
-        <Route path='/search' element={<Search />} />
-        <Route element={<PrivateRoute />}>
-          <Route path='/dashboard' element={<Dashboard />} />
-        </Route>
-        <Route element={<OnlyAdminPrivateRoute />}>
-          <Route path='/create-post' element={<CreatePost />} />
-          <Route path='/update-post/:postId' element={<UpdatePost />} />
-        </Route>
-
-        <Route path='/projects' element={<Projects />} />
-        <Route path='/post/:postSlug' element={<PostPage />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/projects" element={<Projects />} />
+        
       </Routes>
-      <Footer />
-    </BrowserRouter>
-  );
+    </div>
+  )
 }
+
+export default App
