5cbac8a4b99019bdb131c0ceeb1888b2be03b142
theNahraf <farhan.ug23@nsut.ac.in>
2025-02-04 01:57:13 +0530

    change in port

--- api/index.js
+++ api/index.js
@@ -52,7 +52,7 @@ app.use((err, req, res, next) => {
 });
 
 // Start Server (Use PORT from Render)
-const PORT =  3000;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+
+app.listen(3000, () => {
+    console.log(`Server is running on port 3000`);
 });
